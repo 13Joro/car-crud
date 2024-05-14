39735eb19dcdb98c1c78a77469e08e81a84aed9d
39 Joro <kajkcanlas.student@ua.edu.ph>
2024-05-14 23:51:30 +0800

    first commit

---
+++ src/main/java/com/canlas/car/demo/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package com.canlas.car.demo.repository;
+import com.canlas.car.demo.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
+
